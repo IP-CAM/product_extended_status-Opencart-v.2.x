4b77bf8ccf0c85190d05e522dca9b11c5bd7c691
Timur <borodatimur@gmail.com>
2020-06-05 03:04:03 +0300

    fix bug

--- upload/catalog/controller/extension/module/extended_status.php
+++ upload/catalog/controller/extension/module/extended_status.php
@@ -13,64 +13,65 @@ class ControllerExtensionModuleExtendedStatus extends Controller {
             $data['stock'] = $custom_status['name'];
             $data['module_name'] = $this->config->get('extended_status_name') ? $this->config->get('extended_status_name') : '';
 
-            $current_day = $not_changed_day = (int)date('j');
-
-            if( $this->config->get('extended_status_time') && $this->compareTime(date('H:i'), $this->config->get('extended_status_time')) ) {
-                $current_day += 1;
+            if( $i = $this->checkDateToHolidays(0) ) {
+                $current_day_increment = $i;
+            } elseif( $this->config->get('extended_status_time') && $this->compareTime(date('H:i'), $this->config->get('extended_status_time')) ) {
+                $current_day_increment = $this->checkDateToHolidays(1);
+            } else {
+                $current_day_increment = 0;
             }
 
-            $current_day = $this->checkDateToHolidays($current_day);
 
             $data['deliveries'] = array();
 
             foreach( $custom_status['deliveries'] as $delivery ) {
-                if( $delivery['day_job'] ) {
-                    if( $delivery['day_holiday'] !== '' && $current_day > (int)date('j') ) {
+                if( $delivery['day_job'] !== '' ) {
+                    if( $current_day_increment && $delivery['day_holiday'] !== '' ) {
                         $days = explode('+', $delivery['day_holiday']);
                     } else {
                         $days = explode('+', $delivery['day_job']);
                     }
 
-                    $day_from = $this->checkDateToHolidays(( $current_day + (int)$days[0] ));
-                    $day_from_diff = $day_from - $not_changed_day > 0 ? $day_from - $not_changed_day : 0;
-
-                    $month_from = date('n', strtotime('+' . $day_from_diff . ' day'));
+                    $day_from = $this->checkDateToHolidays($current_day_increment + (int)$days[0], '$day_from');
+                    $month_from = date('n', strtotime('+' . ( $day_from ) . ' day'));
 
                     if( count($days) > 1 && $days[1] > 0 ) {
                         $day_to = $this->checkDateToHolidays(( $day_from + (int)$days[1] ));
-                        $day_to_diff = $day_to - $day_from - $not_changed_day > 0 ? $day_to - $day_from - $not_changed_day : 0;
                     } else {
                         $day_to = 0;
-                        $day_to_diff = 0;
                     }
-//                    $this->log->write([ $day_from, $not_changed_day, $day_from_diff, $day_to_diff ]);
-                    if( $day_from_diff === 0 ) {
+
+                    if( $day_from === 0 ) {
                         $day_from = $this->language->get('today');
-                        if( $day_to_diff === 1 ) {
+                        if( $day_to === 1 ) {
                             $day_to = $this->language->get('tomorrow');
                         }
-                    } elseif( $day_from_diff === 1 ) {
+                    } elseif( $day_from === 1 ) {
                         $day_from = $this->language->get('tomorrow');
-                        if( $day_to_diff === 1 ) {
+                        if( $day_to === 1 ) {
                             $day_to = 0;
                         }
                     }
 
                     if( $day_to ) {
-                        $month_to = date('n', strtotime('+' . $day_to_diff . ' day'));
+                        $month_to = date('n', strtotime('+' . $day_to . ' day'));
+
+                        if( is_numeric($day_from) ) {
+                            $day_from = date('j', strtotime('+' . $day_from . ' day'));
+                        }
 
                         if( $month_from === $month_to && is_numeric($day_to) ) {
-                            $date = sprintf($this->language->get('from_to_day'), $day_from, $day_to, $this->language->get('month_' . $month_from));
+                            $date = sprintf($this->language->get('from_to_day'), $day_from, date('j', strtotime('+' . $day_to . ' day')), $this->language->get('month_' . $month_from));
                         } elseif( $month_from === $month_to && is_string($day_to) ) {
                             $date = sprintf($this->language->get('from_to_day_str'), $day_from, $this->language->get('month_' . $month_from), $day_to, $this->language->get('month_' . $month_to));
                         } else {
                             $date = sprintf($this->language->get('from_to_month'), $day_from, $this->language->get('month_' . $month_from), $day_to, $this->language->get('month_' . $month_to));
                         }
                     } else {
-                        if(is_string($day_from)) {
+                        if( is_string($day_from) ) {
                             $date = $day_from;
                         } else {
-                            $date = sprintf($this->language->get('from_day'), $day_from, $this->language->get('month_' . $month_from));
+                            $date = sprintf($this->language->get('from_day'), date('j', strtotime('+' . $day_from . ' day')), $this->language->get('month_' . $month_from));
                         }
                     }
                 } else {
@@ -94,11 +95,11 @@ class ControllerExtensionModuleExtendedStatus extends Controller {
         return ( (float)str_replace(':', '.', $current_time) > (float)str_replace(':', '.', $setting_time) );
     }
 
-    protected function checkDateToHolidays( $day ) {
-        if( strpos($this->holidays[date('n', strtotime('+' . $day . ' day'))] . ',', $day . ',') !== false ) {
-            $day = $this->checkDateToHolidays($day += 1);
+    protected function checkDateToHolidays( $i = 0) {
+        while( strpos(',' . $this->holidays[date('n', strtotime('+' . $i . ' day'))] . ',', ',' . date('j', strtotime('+' . $i . ' day')) . ',') !== false ) {
+            $i++;
         }
 
-        return $day;
+        return $i;
     }
 }
